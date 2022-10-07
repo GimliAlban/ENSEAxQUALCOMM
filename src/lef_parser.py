82226a9cc6c66b37c07011b1cfa190754cf76539
nicobld <belland.nicolas77@gmail.com>
2022-10-07 17:15:51 +0200

    commit

--- src/lef_parser.py
+++ src/lef_parser.py
@@ -5,14 +5,20 @@ Lef Parser class.
 
 from typing import Iterable, Tuple
 
+import re
+
 
 class LefPort:
     """
     Class to describe a lef port.
     """
 
-    def __init__(self, port_name: str) -> None:
+    def __init__(self, port_name: str, direction: str, use: str, layer: str, polygon: Tuple[Tuple]) -> None:
         self.port_name = port_name
+        self.direction = direction
+        self.use = use
+        self.layer = layer
+        self.polygon = polygon
 
     def get_name(self) -> str:
         """Get the name of the port.
@@ -28,6 +34,7 @@ class LefPort:
         Returns:
             str: Direction of the pin, can be INPUT, OUTPUT or INOUT.
         """
+        return self.direction
 
     def get_use(self) -> str:
         """Get the use of the pin.
@@ -35,6 +42,7 @@ class LefPort:
         Returns:
             str: Use of the pin, can be SIGNAL, GROUND or POWER.
         """
+        return self.use
 
     def get_layer(self) -> str:
         """Get the layer associated with the pin.
@@ -42,6 +50,7 @@ class LefPort:
         Returns:
             str: Layer name.
         """
+        return self.layer
 
     def get_polygon(self) -> Tuple[Tuple]:
         """Get the polygon of the port.
@@ -49,6 +58,7 @@ class LefPort:
         Returns:
             Tuple[Tuple]: Tuple of points.
         """
+        return self.polygon
 
 
 class LefCell:
@@ -56,8 +66,9 @@ class LefCell:
     Class to describe a lef cell.
     """
 
-    def __init__(self, cell_name: str) -> None:
+    def __init__(self, cell_name: str, ports: Iterable[LefPort]) -> None:
         self.cell_name = cell_name
+        self.ports = ports
 
     def get_name(self) -> str:
         """Get the name of the cell.
@@ -80,7 +91,7 @@ class LefCell:
         Returns:
             Iterable[LefPort]: Iterable of LefPort in the cell.
         """
-
+        return self.ports
 
 class LefParser:
     """
@@ -96,3 +107,70 @@ class LefParser:
         Returns:
             Iterable[LefCell]: Iterable of LefCell in the lef file.
         """
+        with open(self.lef_file) as lef_file:
+            lef_data = lef_file.read()
+
+        lefcells = []
+        lefports = []
+        end = 0
+
+#search = re.search(r'PIN\s(?P<name>(\w|\_)+)\n\s+DIRECTION\s(?P<direction>\w+)\s;\n\s+USE\s(?P<use>\w+)\s;\n\s+PORT\n\s+LAYER\s(?P<layer>\w+)\s;\n\s+RECT\s(?P<rect>[0-9\.\s]+);', lef_data[end:])
+        
+        while(1):
+            search = re.search(r'MACRO\s(?P<name>(\w|\_)+)', lef_data[end:])
+            if (search == None):
+                break
+            cellname = search.group('name')
+            end = search.end()
+
+            endmacro = re.search(r'END MACRO', lef_data[end:]).start() + end
+
+            print(endmacro)
+  
+            i = 0
+
+            while(1):
+                
+                search = re.search(r'PIN\s(?P<g>(\w|\_)+)', lef_data[end:])
+                name = search.group('g')
+                end += search.end()
+
+
+                search = re.search(r'DIRECTION\s(?P<g>(\w|\_)+)', lef_data[end:])
+                direction = search.group('g')
+                end += search.end()
+
+
+                search = re.search(r'USE\s(?P<g>(\w|\_)+)', lef_data[end:])
+                use = search.group('g')
+                end += search.end()
+
+                
+                search = re.search(r'LAYER\s(?P<g>(\w|\_)+)', lef_data[end:])
+                layer = search.group('g')
+                end += search.end()
+
+                
+                search = re.search(r'RECT\s(?P<g>[0-9\.\s]+)', lef_data[end:])
+                tmp_polygon = search.group('g').split(' ')
+                polygon = ((tmp_polygon[0], tmp_polygon[1]),(tmp_polygon[2], tmp_polygon[3]))
+                end += search.end()
+
+                end += re.search(r'END\s(\w|\_)+\n', lef_data[end:]).end()
+                end += re.search(r'END\s(\w|\_)+\n', lef_data[end:]).end()
+
+                lefports.append(LefPort(name,direction,use,layer,polygon))
+
+                if (end == endmacro):
+                    break
+
+        lefcells.append(LefCell(cellname, lefports))
+
+
+
+
+
+if __name__ == "__main__":
+    lefparser = LefParser("lef_files/1_cells_4_pins.lef")
+
+    lefparser.get_cells()
